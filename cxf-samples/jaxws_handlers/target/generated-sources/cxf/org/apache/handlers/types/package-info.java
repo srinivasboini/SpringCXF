617eb78c9ddaa71f6998613c5a08cafd52932a0b
srinivas boini <srinivasboini7@gmail.com>
2017-12-22 17:56:49 +0800

    ok

---
+++ cxf-samples/jaxws_handlers/target/generated-sources/cxf/org/apache/handlers/types/package-info.java
@@ -0,0 +1,2 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://apache.org/handlers/types", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package org.apache.handlers.types;
